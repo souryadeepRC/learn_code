fcc94826002ab913217987ca5494a0973c5326a0
Souryadeep Roy Chowdhury <thecreativeworldforall@gmail.com>
2026-08-08 22:25:16 +0530

    Change Webpack config

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: [],
-  allowedDevOrigins: ['192.168.1.5', '192.168.1.9'],
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  allowedDevOrigins: [],
 };
 
 export default nextConfig;
